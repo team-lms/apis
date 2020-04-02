3081468e6127df0838fbabb39dd5572197d2861f
Rohit Sharma <rohit2sharma95@gmail.com>
2020-04-02 10:42:05 +0530

    Updated eslint command
    And fixed eslint errors

--- src/api/v1/middlewares/auth.middleware.js
+++ src/api/v1/middlewares/auth.middleware.js
@@ -3,13 +3,13 @@ const { StatusCodeConstants, MessageCodeConstants } = require('../../../constant
 const { Response } = require('../../../utils');
 
 module.exports = {
+  // eslint-disable-next-line consistent-return
   checkAuth: async (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ').pop().trim();
       const { data: hashInfo } = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
+      req.userInfo = hashInfo;
       next();
-      return token;
     } catch (error) {
       return res.status(StatusCodeConstants.UNAUTHORIZED).json(Response.sendError(
         MessageCodeConstants.UNAUTHORIZED_USER,

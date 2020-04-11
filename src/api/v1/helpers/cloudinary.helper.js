f63c38074d76de49e84167dd8e23d4bdbf60565d
Rohit Sharma <rohit2sharma95@gmail.com>
2020-04-11 21:25:47 +0530

    used promisify from util to make the unLink as promise

--- src/api/v1/helpers/cloudinary.helper.js
+++ src/api/v1/helpers/cloudinary.helper.js
@@ -1,5 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
+const { promisify } = require('util');
 
 module.exports = {
   upload: async ({ filePath, tags }) => {
@@ -9,7 +10,7 @@ module.exports = {
       api_secret: process.env.CL_API_SECRET
     });
     const cloudinaryResonse = await cloudinary.uploader.upload(filePath, { tags });
-    (async () => fs.unlinkSync(filePath))();
+    promisify(fs.unlink)(filePath);
     return cloudinaryResonse;
   }
 };

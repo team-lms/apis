9c6748906fa45625937386d0377ab3fbf75de929
Rohit Sharma <rohit2sharma95@gmail.com>
2020-04-26 02:49:24 +0530

    Added eslint:recommended rules in eslintrc.json
    And fixed the eslint rule in the sequelize models' index file

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
-/* eslint-disable import/no-dynamic-require */
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require('../config/config.json')[env];
+
 const db = {};
 
 let sequelize;

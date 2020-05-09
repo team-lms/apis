73ff0efea8adffcfe843ce28b16502bdb2a60062
Siju Samson <sijusamson@gmail.com>
2020-05-09 19:36:20 +0530

    Applied Middleware
    Middleware implemented in Supervisor

--- src/api/v1/routes/supervisor.routes.js
+++ src/api/v1/routes/supervisor.routes.js
@@ -1,9 +1,25 @@
 const router = require('express').Router();
 const { SupervisorController } = require('../controllers');
+const { AuthMiddleware } = require('../middlewares');
+const { AccessConstants } = require('../../../constants');
 
-router.get('/', SupervisorController.getAllSuperVisors);
-router.post('/', SupervisorController.createASupervisor);
-router.patch('/:id', SupervisorController.updateSupervisorById);
-router.delete('/:id', SupervisorController.deleteSupervisorById);
+router.use(AuthMiddleware.checkAuth);
+
+router.get('/',
+  AuthMiddleware.checkAuthByRole(AccessConstants.SUPERVISOR.GET_ALL),
+  SupervisorController.getAllSuperVisors);
+
+router.post('/',
+  AuthMiddleware.checkAuthByRole(AccessConstants.SUPERVISOR.CREATE),
+  SupervisorController.createASupervisor);
+
+router.patch('/:id',
+  AuthMiddleware.checkAuthByRole(AccessConstants.SUPERVISOR.UPDATE),
+  SupervisorController.updateSupervisorById);
+
+
+router.delete('/:id',
+  AuthMiddleware.checkAuthByRole(AccessConstants.SUPERVISOR.DELETE),
+  SupervisorController.deleteSupervisorById);
 
 module.exports = router;
--- src/constants/access-constants.js
+++ src/constants/access-constants.js
@@ -5,7 +5,7 @@ module.exports = Object.freeze({
     CREATE: [RolesConstants.ADMIN, RolesConstants.HR],
     GET_ALL: [RolesConstants.ADMIN, RolesConstants.HR],
     UPDATE: [RolesConstants.ADMIN, RolesConstants.HR],
-    DELETE: [RolesConstants.ADMIN, RolesConstants]
+    DELETE: [RolesConstants.ADMIN, RolesConstants.HR]
   }),
 
   HUMAN_RESOURCE: Object.freeze({
@@ -13,6 +13,13 @@ module.exports = Object.freeze({
     GET_ALL: [RolesConstants.ADMIN],
     UPDATE: [RolesConstants.ADMIN],
     DELETE: [RolesConstants.ADMIN]
+  }),
+
+  SUPERVISOR: Object.freeze({
+    CREATE: [RolesConstants.ADMIN, RolesConstants.HR],
+    GET_ALL: [RolesConstants.ADMIN, RolesConstants.HR],
+    UPDATE: [RolesConstants.ADMIN, RolesConstants.HR],
+    DELETE: [RolesConstants.ADMIN, RolesConstants.HR]
   })
 
 });

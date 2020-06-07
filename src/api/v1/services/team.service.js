1508ed8168af75e350bd54b1b8f62923939968cc
Siju Samson <sijusamson@gmail.com>
2020-06-07 15:45:38 +0530

    ESLint issue

--- src/api/v1/services/team.service.js
+++ src/api/v1/services/team.service.js
@@ -1,6 +1,5 @@
 const { Op } = require('sequelize');
 const { Team, TeamAssociation } = require('../../../../models');
-const { StatusConstants } = require('../../../constants');
 
 const TeamsService = {
 

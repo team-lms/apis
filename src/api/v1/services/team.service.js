44273a6568000507876137a0b99edf0c5731b3db
Siju Samson <sijusamson@gmail.com>
2020-07-25 11:12:46 +0530

    Changes for Team Listing

--- src/api/v1/services/team.service.js
+++ src/api/v1/services/team.service.js
@@ -4,31 +4,35 @@ const { Team, User } = require('../../../../models');
 module.exports = {
 
   /**
-   * Get team by team id
-   */
+     * Get team by team id
+     */
   getTeamById: async (teamId) => Team.findOne({ id: teamId }),
 
   /**
-   * Get the teams
-   */
+     * Get the teams
+     */
   getAllTeams: async ({
-    offset, limit, sortBy, sortType
+    offset,
+    limit,
+    sortBy,
+    sortType
   }) => Team.findAndCountAll({
-    order: [[sortBy, sortType]],
+    order: [
+      [sortBy, sortType]
+    ],
     offset,
     limit,
     attributes: { exclude: ['updatedAt', 'deletedAt'] },
     include: [{
       model: User,
       as: 'users',
-      through: { attributes: [] },
       attributes: ['id', 'firstName', 'middleName', 'lastName', 'email', 'phoneNumber', 'role']
     }]
   }),
 
   /**
-   * Get team by team name
-   */
+     * Get team by team name
+     */
   findTeamByTeamName: async (teamName, teamId = null) => {
     if (!teamName) {
       return null;
@@ -36,38 +40,41 @@ module.exports = {
     return Team.findOne({
       where: {
         teamName,
-        ...(teamId && { id: { [Op.ne]: teamId } })
+        ...(teamId && {
+          id: { [Op.ne]: teamId }
+        })
       },
       paranoid: true
     });
   },
 
   /**
-   * Create a new team
-   */
+     * Create a new team
+     */
   createTeam: async (teamToBeCreated, transaction = null) => Team.create(teamToBeCreated, {
     ...(transaction && { transaction })
   }),
 
   /**
-   * Update a team by id
-   */
+     * Update a team by id
+     */
   updateATeam: async (teamToBeUpdated, id, transaction) => Team.update(
-    teamToBeUpdated,
-    {
+    teamToBeUpdated, {
       where: { id },
       ...(transaction && { transaction })
     }
   ),
 
   /**
-   * Delete a team by id
-   */
+     * Delete a team by id
+     */
   deleteATeam: async (id, transaction = null) => Team.destroy({
     where: {
       [Op.and]: [
         { id },
-        { deletedAt: { [Op.eq]: null } }
+        {
+          deletedAt: { [Op.eq]: null }
+        }
       ]
     },
     ...(transaction && { transaction })
